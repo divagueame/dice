f6c6e6d1fc33a1d24eaf0d380e42d96dc39e1707
martin <mocon@DESKTOP-1GQAV5L>
2021-11-25 15:46:29 +0200

    S

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const rollBtn = document.getElementById('roll-btn');
 rollBtn.addEventListener('click', ()=>{
   const dice = document.querySelector('.dice');
-  dice.classList.add('dice-animation');
+  // dice.classList.remove('dice-basic-rotation');
   dice.classList.add('dice-animation1');
+  dice.classList.add('dice-animation');
+
   setTimeout(()=>{
     dice.classList.remove('dice-animation');
     dice.classList.remove('dice-animation1');
-  },300)
+    // dice.classList.add('dice-basic-rotation');
+  },3100)
 })
